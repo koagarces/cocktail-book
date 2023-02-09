20a3d7e75228e6cce3534d5d84368682adba04a7
Koa Garces <koajgarces@yahoo.com>
2023-02-08 22:41:01 -0800

    got pagination function working

--- src/components/Pagenation.jsx
+++ src/components/Pagenation.jsx
@@ -1,38 +1,31 @@
 import React, { useState } from "react";
 
-const Pagination = ({ totalPages, pageNeighbours = 2 }) => {
-  const [currentPage, setCurrentPage] = useState(1);
+const Pagination = ({ totalPages, currentPage, setCurrentPage }) => {
+  let numArr = [];
 
   const handleClick = (page) => {
     setCurrentPage(page);
   };
 
-  const totalNumbers = pageNeighbours * 2 + 3;
-  const totalBlocks = totalNumbers + 2;
-  if (totalPages > totalBlocks) {
-    const startPage = Math.max(2, currentPage - pageNeighbours);
-    const endPage = Math.min(totalPages - 1, currentPage + pageNeighbours);
-
-    let pages = [...Array(endPage + 1 - startPage).keys()].map(
-      (i) => startPage + i
-    );
-
-    pages = [1, ...pages, totalPages];
-    return (
-      <div>
-        {pages.map((page) => (
-          <button
-            key={page}
-            onClick={() => handleClick(page)}
-            disabled={currentPage === page}
-          >
-            {page}
-          </button>
-        ))}
-      </div>
-    );
+  for (let i = 1; i < totalPages + 1; i++) {
+    numArr.push(i);
   }
-  return null;
+  console.log(numArr);
+  return (
+    <div>
+      {numArr.map((page) => (
+        <button
+          key={page}
+          onClick={() => handleClick(page)}
+          disabled={currentPage === page}
+        >
+          {page}
+        </button>
+      ))}
+    </div>
+  );
 };
+//   return null;
+// };
 
 export default Pagination;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,17 +5,34 @@ import Pagination from "./Pagenation";
 const Search = () => {
   const [searchBar, setSearchBar] = useState("");
   const [cocktails, setCocktails] = useState([]);
-  let searchSubmitted = false;
+  const [currentPage, setCurrentPage] = useState(1);
+  let head = 0;
+  let tail = 5;
+  let totalPages = Math.ceil(cocktails.length / 5);
 
-  console.log(searchBar);
   const handleChange = (e) => {
     setSearchBar(e.target.value);
   };
 
   const fetchCocktails = async (event) => {
     event.preventDefault();
+    setCurrentPage(1);
+    head = 0;
+    tail = 5;
     setCocktails(await getCocktailByName(searchBar));
   };
+
+  if (currentPage > 1) {
+    head = (currentPage - 1) * 5;
+    tail = head + 5;
+  } else if (currentPage === 1) {
+    head = 0;
+    tail = 5;
+  }
+  let onePage = cocktails.slice(parseInt(head), parseInt(tail));
+
+  console.log("head", head);
+  console.log("onePage", onePage);
 
   return (
     <form onSubmit={fetchCocktails}>
@@ -26,10 +43,10 @@ const Search = () => {
         placeholder="Find a CockTail!"
         onChange={(e) => handleChange(e)}
       />
-      <input type="submit" value="Submit" disabled={false} />
+      <input type="submit" value="Submit" />
       <div>
         <ul>
-          {cocktails.map((cocktail) => (
+          {onePage.map((cocktail) => (
             <div>
               <li key={cocktail.idDrink}>{cocktail.strDrink}</li>
               <img src={cocktail.strDrinkThumb} />
@@ -37,7 +54,13 @@ const Search = () => {
           ))}
         </ul>
       </div>
-      <Pagination />
+      <Pagination
+        totalPages={totalPages}
+        head={head}
+        tail={tail}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </form>
   );
 };
